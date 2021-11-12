f95e1418ce2f8f4c4ba9443aac82687682f4f653
era-muriithi <eramuriithi@gmail.com>
2021-11-12 13:01:55 +0300

    complete safety lights tab content

--- app/src/main/java/com/example/dashboardlights/Warning.java
+++ app/src/main/java/com/example/dashboardlights/Warning.java
@@ -20,10 +20,12 @@ public class Warning extends Fragment {
     String [] lightname = {"Check Engine", "Brake Warning Light", "Battery Charge Warning Light", "Oil Pressure Warning light",
             "Engine Temperature Warning Light", "Steering Wheel Lock", "Trailer Tow Hitch Warning", "Parking Brake Light", "Power Steering Warning Light",
     "Seat Belt Indicator", "Master Warning Light", "Distance Warning", "Catalytic Converter Warning", "Brake Lights Warning", "Automatic Gearbox Warning",
-    "All Wheel Drive (AWD/4WD)", "Airbag Indicator", "4 Wheel Drive (4WD) LOCK Indicator Light"};
+    "All Wheel Drive (AWD/4WD)", "Airbag Indicator", "4 Wheel Drive (4WD) LOCK Indicator Light", "Door Ajar", "Hood/Bonnet Open", "Low Fuel Level", "Adaptive Suspension Dampers",
+    "Key Fob Battery Low", "Ignition Switch Warning", "Glow Plug Indicator", "Fuel Filter Warning", "Water Fluid Filter Warning"};
     int [] images = {R.drawable.checklight, R.drawable.brakefluid, R.drawable.battery, R.drawable.oilpressure, R.drawable.temperaturewarning,
     R.drawable.wheellock, R.drawable.tow, R.drawable.parkingbrake, R.drawable.powersteering, R.drawable.seatbelt, R.drawable.master, R.drawable.distance,
-    R.drawable.catalytic, R.drawable.brakelight, R.drawable.autogearbox, R.drawable.awd, R.drawable.airbagwarning, R.drawable.awdlock};
+    R.drawable.catalytic, R.drawable.brakelight, R.drawable.autogearbox, R.drawable.awd, R.drawable.airbagwarning, R.drawable.awdlock, R.drawable.doorajar, R.drawable.bonnet,
+    R.drawable.lowfuel, R.drawable.suspension, R.drawable.keyfob, R.drawable.ignination, R.drawable.glow, R.drawable.fuelfilter, R.drawable.water};
     Context context;
     ListViewAdapter adapter;
 
