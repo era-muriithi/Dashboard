21b5130e26b1949296f6e2d7a710695ce8e03c7e
era-muriithi <eramuriithi@gmail.com>
2021-11-20 22:44:24 +0300

    Linked list items to details activity

--- app/src/main/java/com/example/dashboardlights/CheckEngine.java
+++ app/src/main/java/com/example/dashboardlights/CheckEngine.java
@@ -1,14 +1,50 @@
 package com.example.dashboardlights;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckEngine extends AppCompatActivity {
+    TextView lighttitle;
+    ImageView imageView;
+    ListView lightlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkengine_activity);
+
+
+        lighttitle = findViewById(R.id.light_title);
+        imageView = findViewById(R.id.lightimage);
+        lightlist = findViewById(R.id.lightslist);
+
+        Intent intent = getIntent();
+        String receivedName = intent.getStringExtra("name");
+        int receivedImage = intent.getIntExtra("image", 0);
+        lighttitle.setText(receivedName);
+        imageView.setImageResource(receivedImage);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
-}
+    }
+
--- app/src/main/java/com/example/dashboardlights/Safety.java
+++ app/src/main/java/com/example/dashboardlights/Safety.java
@@ -10,15 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class Safety extends Fragment {
     ListView listView;
-    String [] lightname = {"Transmission Temperature", "Tire Pressure Warning Light", "Traction Control Off", "Traction Control Light", "Service Soon", "Security Alert",
-    "Reduced Power", "PowerTrain Fault", "Gas/Fuel Cap", "ESP Fault/Traction Control Malfunction", "Electric Park Brake", "Low Brake Fluid"};
-    int [] images = {R.drawable.transmissiontempertaure, R.drawable.lowtirepressure, R.drawable.tractionoff, R.drawable.tractionon, R.drawable.servicesoon, R.drawable.securityalert,
-    R.drawable.reducedpower, R.drawable.powertrain, R.drawable.fuelcap, R.drawable.espfault, R.drawable.electricparkbrake, R.drawable.brakefluidlow};
+    String[] lightname = {"Transmission Temperature", "Tire Pressure Warning Light", "Traction Control Off", "Traction Control Light", "Service Soon", "Security Alert",
+            "Reduced Power", "PowerTrain Fault", "Gas/Fuel Cap", "ESP Fault/Traction Control Malfunction", "Electric Park Brake", "Low Brake Fluid"};
+    int[] images = {R.drawable.transmissiontempertaure, R.drawable.lowtirepressure, R.drawable.tractionoff, R.drawable.tractionon, R.drawable.servicesoon, R.drawable.securityalert,
+            R.drawable.reducedpower, R.drawable.powertrain, R.drawable.fuelcap, R.drawable.espfault, R.drawable.electricparkbrake, R.drawable.brakefluidlow};
     ListViewAdapter adapter;
 
 
@@ -31,21 +34,50 @@ public class Safety extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_safety, container, false);
         listView = rootView.findViewById(R.id.lightslist);
+        CustomAdapter customAdapter = new CustomAdapter();
 
         adapter = new ListViewAdapter(getActivity(), lightname, images);
-        listView.setAdapter(adapter);
+        listView.setAdapter(customAdapter);
 
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            if (position == 0) {
-                Intent myIntent = new Intent(view.getContext(), CheckEngine.class);
-                startActivityForResult(myIntent, 0);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Intent intent = new Intent(getContext(), CheckEngine.class);
+                intent.putExtra("name", lightname[i]);
+                intent.putExtra("image", images[i]);
+                startActivity(intent);
             }
-
-
         });
-
         return rootView;
+    }
 
+    private class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View view1 = getLayoutInflater().inflate(R.layout.custom_item, null);
+            //getting view in row_data
+            TextView name = view1.findViewById(R.id.lightname);
+            ImageView image = view1.findViewById(R.id.img);
+
+            name.setText(lightname[i]);
+            image.setImageResource(images[i]);
+            return view1;
+        }
 
     }
 }
